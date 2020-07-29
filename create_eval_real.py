f682a3b7eaed060e404386d6eebed49b218d9958
daryachyzhyk <d.chyzhyk@lookiero.com>
2020-07-30 00:50:40 +0200

    add dates to analyze

--- create_eval_real.py
+++ create_eval_real.py
@@ -8,6 +8,7 @@ Script to create the table of the real data last week:
 
 The scrip take the date for analyze as a previous Monday to the day of run the code
 '''
+
 import os
 import glob
 import pandas as pd
@@ -19,67 +20,105 @@ import datetime
 
 ####################################################################################################################
 # Date to analyze
-day_today = datetime.date.today()
-date_actual = day_today - datetime.timedelta(days = 7 + day_today.weekday())
+day_today = datetime.datetime.now()
+date_start = day_today - datetime.timedelta(days = 7 + day_today.weekday())
 
-fecha_stock_actual_start_str = '2020-07-13'
-# fecha_stock_actual_end_str = '2020-07-19'
-#
-# fecha_compra = '2020-06-17'
-#
-# fecha_pedido_start = '2020-06-17'
-# fecha_pedido_end = '2020-06-30'
-#
-#
-# familias_interes = ['BLUSA', 'CAMISETA', 'FALDA', 'JUMPSUIT', 'SHORT', 'TOP', 'VESTIDO']
+date_start_str = datetime.datetime.strftime(date_start, '%Y-%m-%d')
+date_end = date_start + datetime.timedelta(days = 6)
 
-###################################################################################################################
-# settings
+date_end_str = datetime.datetime.strftime(date_end, '%Y-%m-%d')
 
-dict_fechas = {'2020-07-13': ['2020-06-17', '2020-06-16'],
-               '2020-07-13': ['2020-06-17', '2020-06-16'],
-               '2020-07-20': ['2020-06-17', '2020-06-16'],
-               '2020-07-27': ['2020-06-17', '2020-06-16'],
-               '2020-08-03': ['2020-07-03', '2020-07-02'],
-               '2020-08-10': ['2020-07-03', '2020-07-02'],
-               '2020-08-17': ['2020-07-03', '2020-07-02'],
-               '2020-08-24': ['2020-07-03', '2020-07-02']}
-dic_clima = {0.: 'cold',
-             0.5: 'cold_soft_cold',
-             1.: 'soft_cold',
-             1.5: 'soft_cold_soft_warm',
-             2.: 'soft_warm',
-             2.5: 'soft_warm_warm',
-             3.:  'warm'}
 
-dic_clima = {0.: '0_invierno',
-             0.5: '0.5_invierno',
-             1.: '1_invierno',
-             1.5: '1.5_entretiempo',
-             2.: '2_verano',
-             2.5: '2.5_verano',
-             3.:  '3_verano'}
+# fecha_stock_actual_start_str = '2020-07-13'
 
-dic_clima = {0.: '0_inv',
-             0.5: '0.5_inv',
-             1.: '1_inv',
-             1.5: '1.5_ent',
-             2.: '2_ver',
-             2.5: '2.5_ver',
-             3.:  '3_ver'}
+
+######################################################################################3
 
 # path
 stock_folder = ('/var/lib/lookiero/stock/snapshots')
 
 productos_file = ('/var/lib/lookiero/stock/stock_tool/productos_preprocessed.csv.gz')
 
-stuart_folder = ('/var/lib/lookiero/stock/stock_tool/stuart')
+# TODO: change to stock server
+path_save = ('/home/darya/Documents/Reports/2020-07-17-kpi-roturas-particular-case')
 
 
-path_results = ('/home/darya/Documents/Reports/2020-07-17-kpi-roturas-particular-case')
+#########################################################
+########################################
+# stock actual
+# stock_actual_file = []
+
+# TODO: create df['reference', 'clima']
+
+df_stock_all = pd.DataFrame([])
+
+for i in range(delta_fecha_stock_actual.days + 1):
+    day = fecha_stock_actual_start + datetime.timedelta(days=i)
+    print(day)
+
+    stock_fecha = day.strftime('%Y%m%d')
+
+    # list_dates =
+
+    # stock_fecha = datetime.datetime.strptime(fecha_compra, '%Y-%m-%d').strftime('%Y%m%d')
+
+    stock_file = sorted(glob.glob(os.path.join(stock_path, stock_fecha + '*')))[0]
+
+    print(stock_file)
+
+
+
+
+    query_stock_text = 'real_stock > 0 and active > 0'
+
+    df_stock_day = pd.read_csv(stock_file,
+                               usecols=['reference', 'family', 'real_stock', 'active']
+                               ).query(query_stock_text)
+    df_stock_day['date'] = day
+
+    df_stock_all = df_stock_all.append(df_stock_day)
+
+
+df_stock_all = df_stock_all.rename(columns={'real_stock': 'stock_real_week'})
+#######################################################
+# info de cada prenda
+list_reference_stock = df_stock_all["reference"].to_list()
+query_product_text = 'reference in @list_reference_stock'
+
+
+df_productos = pd.read_csv(productos_file,
+                           usecols=['reference', 'family_desc', 'size', 'clima'] # , 'clima_grupo'
+                           ).query(query_product_text)
+
+#########################################################
+# stock actual añadir familia, talla, clima
+
+df_stock_reference = pd.merge(df_stock_all, df_productos, on='reference', how='left')
+
+
+
+df_stock_familia_talla = df_stock_reference.groupby(['family_desc', 'size']).agg({'stock_real_week': 'sum',
+                                                                                  'date': 'last'}).reset_index()
+
+df_stock_familia_clima = df_stock_reference.groupby(['family_desc', 'clima']).agg({'stock_real_week': 'sum',
+                                                                                   'date': 'last'}).reset_index()
+
+df_stock_familia_clima['clima_desc'] = df_stock_familia_clima['clima'].replace(dic_clima)
+df_stock_familia_clima['clima'] = df_stock_familia_clima['clima'].astype(str)
+
+df_stock_familia_talla['stock_real'] = (df_stock_familia_talla['stock_real_week'] / 7)
+
+df_stock_familia_clima['stock_real'] = (df_stock_familia_clima['stock_real_week'] / 7)
+
+
+
+# df_stock_familia_talla['stock_mean'] = (df_stock_familia_talla['real_stock'] / 7).round(0)
+# df_stock_familia_talla['stock_mean1'] = (df_stock_familia_talla['real_stock'] / 7).apply(np.ceil)
+
 
 ######
 # fechas
+date_stock_start
 fecha_stock_actual_start = datetime.strptime(fecha_stock_actual_start_str, '%Y-%m-%d')
 fecha_stock_actual_end = fecha_stock_actual_start + timedelta(days=6)
 

96a5496f9bf1f86c95f66179e4b07097e8b64cd9
daryachyzhyk <d.chyzhyk@lookiero.com>
2020-08-21 18:38:32 +0200

    extract stuart

--- eval_compra_real_stuart.py
+++ eval_compra_real_stuart.py
@@ -55,8 +55,7 @@ def get_fam_size_clima(references, file=None, drop_duplicates=True, family=True,
     return df
 
 
-
-def get_compra_real(date_start_str, compra_file, compra_dates_file, productos_file):
+def get_compra_real(date_compra_str, compra_file, productos_file):
     # TODO: download compra file from Google Drive
 
     # compra realizada
@@ -65,29 +64,88 @@ def get_compra_real(date_start_str, compra_file, compra_dates_file, productos_fi
     # link between week and compra date
     # compra_dates_file = ('/var/lib/lookiero/stock/stock_tool/kpi/compra/compra_fechas - Sheet1.csv')
 
-    query_compra_date_text = 'week == @date_start_str'
-    df_date_compra = pd.read_csv(compra_dates_file).query(query_compra_date_text)
-    if df_date_compra.empty == False:
-        date_compra_str = df_date_compra['date_compra'].values[0]
+    # query_compra_date_text = 'week == @date_compra_str'
+    # df_date_compra = pd.read_csv(compra_dates_file).query(query_compra_date_text)
+    # if df_date_compra.empty == False:
+    #     date_compra_str = df_date_compra['date_compra'].values[0]
 
-        query_compra_reference_text = 'date_compra == @date_compra_str'
-        df_compra_raw = pd.read_csv(compra_file,
-                                    usecols=['date_compra', 'reference', 'cantidad_pedida']
-                                    ).query(query_compra_reference_text)
+    query_compra_reference_text = 'date_compra == @date_compra_str'
+    df_compra_raw = pd.read_csv(compra_file,
+                                usecols=['date_compra', 'reference', 'cantidad_pedida']
+                                ).query(query_compra_reference_text)
 
-        list_references_compra = df_compra_raw["reference"].to_list()
-        df_compra_products = get_fam_size_clima(list_references_compra, productos_file, drop_duplicates=True)
+    list_references_compra = df_compra_raw["reference"].to_list()
+    df_compra_products = get_fam_size_clima(list_references_compra, productos_file, drop_duplicates=True)
 
-        df_compra_reference = pd.merge(df_compra_raw, df_compra_products, on='reference', how='left')
-        df_compra = df_compra_reference.groupby(['family_desc', 'clima', 'size']).agg({'cantidad_pedida': 'sum'}).reset_index()
+    df_compra_reference = pd.merge(df_compra_raw, df_compra_products, on='reference', how='left')
+    df_compra = df_compra_reference.groupby(['family_desc', 'clima', 'size']).agg({'cantidad_pedida': 'sum'}).reset_index()
 
-        df_compra['info_type'] = 'pedido'
-        df_compra = df_compra.rename(columns={'cantidad_pedida': 'q'})
+    df_compra['info_type'] = 'pedido'
+    df_compra = df_compra.rename(columns={'cantidad_pedida': 'compra_real'})
 
-    else:
-        print('There is no date ' + date_start_str + '. Please update the data here: ' + compra_dates_file)
+    # else:
+    #     print('There is no date ' + date_compra_str + '. Please update the data here: ' + compra_dates_file)
 
     return df_compra
+
+
+def get_stuart_recommendation(date_compra_str, compra_date_stuart_id_file):
+    df_date_id = pd.read_csv(compra_date_stuart_id_file)
+    date_stuart_str = df_date_id[df_date_id['date_compra']==date_compra_str]['date_stuart'].values[0]
+    date_stuart_datetime = datetime.datetime.strptime(date_stuart_str, '%Y-%m-%d')
+    path_stuart = ('/var/lib/lookiero/stock/stock_tool/stuart')
+    stuart_folder = date_stuart_datetime.strftime('%Y%m%d')
+    stuart_file = os.path.join(path_stuart, stuart_folder, 'stuart_output_todos.csv.gz')
+
+    df_raw = pd.read_csv(stuart_file)
+    df_raw[['family_desc', 'clima']] = df_raw['family_desc-clima'].str.split("-", expand=True)
+    df_raw[['temp', 'size']] = df_raw['clase'].str.split("-", expand=True)
+    df_raw = df_raw.rename({'clase': 'size_desc'})
+    df_stuart = df_raw[['family_desc', 'clima', 'size', 'recomendacion', 'size_desc']]
+
+# run
+date_compra_str = '2020-07-22'
+date_stuart_str = '2020-08-03'
+compra_file = ('/var/lib/lookiero/stock/stock_tool/kpi/compra/compra_reference_quantity - Sheet1.csv')
+productos_file = ('/var/lib/lookiero/stock/stock_tool/productos_preprocessed.csv.gz')
+compra_date_stuart_id_file = ('/var/lib/lookiero/stock/stock_tool/kpi/compra/compra-date-stuart-id - Sheet1.csv')
+
+df_compra_real = get_compra_real(date_compra_str, compra_file, productos_file)
+
+
+#
+# def get_compra_real(date_compra_str, compra_file, compra_dates_file, productos_file):
+#     # TODO: download compra file from Google Drive
+#
+#     # compra realizada
+#     # compra_file = ('/var/lib/lookiero/stock/stock_tool/kpi/compra/compra_referemce_quantity - Sheet1.csv')
+#
+#     # link between week and compra date
+#     # compra_dates_file = ('/var/lib/lookiero/stock/stock_tool/kpi/compra/compra_fechas - Sheet1.csv')
+#
+#     query_compra_date_text = 'week == @date_compra_str'
+#     df_date_compra = pd.read_csv(compra_dates_file).query(query_compra_date_text)
+#     if df_date_compra.empty == False:
+#         date_compra_str = df_date_compra['date_compra'].values[0]
+#
+#         query_compra_reference_text = 'date_compra == @date_compra_str'
+#         df_compra_raw = pd.read_csv(compra_file,
+#                                     usecols=['date_compra', 'reference', 'cantidad_pedida']
+#                                     ).query(query_compra_reference_text)
+#
+#         list_references_compra = df_compra_raw["reference"].to_list()
+#         df_compra_products = get_fam_size_clima(list_references_compra, productos_file, drop_duplicates=True)
+#
+#         df_compra_reference = pd.merge(df_compra_raw, df_compra_products, on='reference', how='left')
+#         df_compra = df_compra_reference.groupby(['family_desc', 'clima', 'size']).agg({'cantidad_pedida': 'sum'}).reset_index()
+#
+#         df_compra['info_type'] = 'pedido'
+#         df_compra = df_compra.rename(columns={'cantidad_pedida': 'q'})
+#
+#     else:
+#         print('There is no date ' + date_compra_str + '. Please update the data here: ' + compra_dates_file)
+#
+#     return df_compra
 
 
 
@@ -103,5 +161,8 @@ compra_date_stuart_id = ('/var/lib/lookiero/stock/stock_tool/kpi/compra/compra-d
 productos_file = ('/var/lib/lookiero/stock/stock_tool/productos_preprocessed.csv.gz')
 
 
-date_start_str = '2020'
-get_compra_real(date_start_str, compra_file, compra_dates_file, productos_file)
+date_compra_str = '2020-07-22'
+get_compra_real(date_compra_str, compra_file, compra_dates_file, productos_file)
+
+
+# date_compra	id_stuart	date_stuart
